07b9e13f2a28c7612d14d8638c96ba8602c5f65e
chouxiaozi <www.undefined@qq.com>
2020-04-27 23:40:00 +0800

    引入elemenu-ui，以及登录页面开发

---
+++ vuehr/src/views/Login.vue
@@ -0,0 +1,74 @@
+<template>
+    <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+        <h3 class="loginTitle">系统登录</h3>
+        <el-form-item prop="username">
+            <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+            <el-input type="text" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-checkbox v-model="checked" class="loginRemember">记住密码</el-checkbox>
+        <el-button type="primary" style="width: 100%;" @click="submitLogin">登录</el-button>
+    </el-form>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+      data(){
+        return{
+            loginForm:{
+                username:'admin',
+                password:'123'
+            },
+            checked:true,
+            rules:{
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' }
+                ]
+            }
+        }
+      },
+      methods:{
+        submitLogin(){
+            this.$refs['loginForm'].validate((valid) => {
+                if (valid) {
+                    this.$message({
+                        message: '登录成功',
+                        type: 'success'
+                    });
+                } else {
+                    this.$message.error('用户名或密码错误');
+                    return false;
+                }
+            });
+        }
+      }
+  }
+</script>
+
+<style>
+    .loginContainer{
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 15px 35px 15px 35px;
+        background: #ffffff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+    .loginTitle{
+        margin: 15px auto 20px auto;
+        text-align: center;
+        color: #505458;
+    }
+    .loginRemember{
+        text-align: left;
+        margin: 0px 0px 15px 0px;
+    }
+
+</style>
