a4e2626154ef84af3ccc848cafcbce88954a7158
chouxiaozi <www.undefined@qq.com>
2020-04-28 22:11:22 +0800

    生成hr，menu等表的代码

---
+++ vhr/src/main/java/com/chouxiaozi/vhr/mapper/HrMapper.java
@@ -0,0 +1,30 @@
+package com.chouxiaozi.vhr.mapper;
+
+import com.chouxiaozi.vhr.model.Hr;
+import com.chouxiaozi.vhr.model.HrExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface HrMapper {
+    int countByExample(HrExample example);
+
+    int deleteByExample(HrExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Hr record);
+
+    int insertSelective(Hr record);
+
+    List<Hr> selectByExample(HrExample example);
+
+    Hr selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Hr record, @Param("example") HrExample example);
+
+    int updateByExample(@Param("record") Hr record, @Param("example") HrExample example);
+
+    int updateByPrimaryKeySelective(Hr record);
+
+    int updateByPrimaryKey(Hr record);
+}
