55319949c0f08e7d1377a95b52932367bc099cea
chouxiaozi <www.undefined@qq.com>
2020-05-31 23:06:34 +0800

    111

--- vhr/src/main/java/com/chouxiaozi/vhr/controller/emp/EmpBasicController.java
+++ vhr/src/main/java/com/chouxiaozi/vhr/controller/emp/EmpBasicController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/emp/basic")
-public class BasicController {
+public class EmpBasicController {
     @Autowired
     EmployeeService employeeService;
     @GetMapping("/")
